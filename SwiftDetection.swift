c6cd7fa8bcc8f054dc4499523591dead7b703d63
P.Gerdenel <47711469+pgerdenel@users.noreply.github.com>
2020-07-22 15:45:47 +0200

    Add files via upload

---
+++ SwiftDetection.swift
@@ -0,0 +1,23 @@
+import XCTest
+@testable import LanguageDetection
+ 
+class LanguageDetectionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testExample() {
+    }
+    
+    func testPerformanceExample() {
+        self.measure {
+        }
+    }
+    
+}
+
