a0f1ac0710a0110567726d11d417b7427576e1b7
P.Gerdenel <47711469+pgerdenel@users.noreply.github.com>
2020-07-22 15:47:36 +0200

    Update JavaDetection.java

--- JavaDetection.java
+++ JavaDetection.java
@@ -1,7 +1,30 @@
 public class JavaDetection
 {
-    // ... 
     public static void main(String[] args)
+    {
+        // Language Detection
+    }
+    public void detect1()
+    {
+        // Language Detection
+    }
+    public void detect1()
+    {
+        // Language Detection
+    }
+    public void detect1()
+    {
+        // Language Detection
+    }
+    public void detect2()
+    {
+        // Language Detection
+    }
+    public void detect2()
+    {
+        // Language Detection
+    }
+    public void detect2()
     {
         // Language Detection
     }
